cef4efdde656ec16188ec932e19d1c247cb7d055
zhaojiabo <2356224342@qq.com>
2025-04-11 14:24:18 +0800

    f

---
+++ xiangmu1/dajiba3/src/components/Person.vue
@@ -0,0 +1,42 @@
+<template>
+<div class="person"><
+<h2>当前求和{{ sum }}</h2>
+<button @click="add">点我sum+1</button>
+<hr>
+<img v-for="(dog,index) in doglist" :src="dog" :key="index">
+<!-- 遍历数组进行图片的展示 -->
+ <button @click="getdog()">再来一只狗</button>
+</div>
+
+
+
+
+
+</template>
+<script lang="ts" setup name="Person1">
+import {ref,reactive} from 'vue'
+import  axious from 'axios'
+//数据
+let sum =ref(0)
+let doglist=reactive([
+    'https://imagines.dog.ceo/breads/pembroke/n02113023_4373.jpg'
+])
+async function getdog() {
+    try{let result=await axious.get('https://dog.ceo/api/bread/pembroke/images/random')
+console.log(result.data)
+    }catch{
+
+    }
+}
+
+function  add(){
+    sum.value+=1  
+}
+</script>
+<style scoped>
+img{
+    height: 100px;
+}
+
+
+</style>
---
+++ 备份/01__我是/Person.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="person">
+      <h2>姓名：{{name}}</h2>
+      <h2>年龄：{{age}}</h2>
+      <button @click="changName">修改名字</button>
+      <button @click="changAge">年龄+1</button>
+      <button @click="showTel">点我查看联系方式</button>
+    </div>
+  </template>
+  
+  <script>
+  
+    // 方法
+    function changName(){
+      name = '李四'//注意：此时这么修改name页面是不变化的
+    }
+    function changAge(){
+      console.log(age)
+      age += 1 //注意：此时这么修改age页面是不变化的
+    }
+    function showTel(){
+      alert(tel)
+    }
+  </script>
